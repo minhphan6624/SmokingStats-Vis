652a64c5e93c9735d021b5d0a88b30c1265f14a3
minhphan6624 <quangminh6624@gmail.com>
2024-04-12 00:23:20 +1000

    work on structure and navbar

---
+++ scripts/navbar.js
@@ -0,0 +1,30 @@
+document.write(`
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">YourSite</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="vis1.html">Chart 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="vis2.html">Chart 2</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="vis3.html">Chart 3</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
+            </li>
+        </ul>
+    </div>
+</div>
+</nav>
+`);
