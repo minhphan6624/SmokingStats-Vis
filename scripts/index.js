055cd32f26fd55d4a2ca95d2eb3b15b756eddee3
Minh Phan <113814582+minhphan6624@users.noreply.github.com>
2024-04-28 17:02:54 +1000

    Common navbar and footer; Initialized SVG

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+width = 800;
+height = 500;
+
+//Creatin the svg
+var svg = d3.select("#vis1")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+
