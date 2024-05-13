b74837156ee0c956c87520ede99fb23190416065
minhphan6624 <quangminh6624@gmail.com>
2024-05-13 19:18:24 +1000

    added svgs for map and stacked bar vis

---
+++ scripts/choropleths.js
@@ -0,0 +1,8 @@
+var w = 500
+var h = 300
+
+//Create SVG element
+var svg = d3.select(".vis2")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,3 @@
-width = 600;
-height = 300;
 
-//Creatin the svg
-var svg = d3.select(".vis")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
 
 
