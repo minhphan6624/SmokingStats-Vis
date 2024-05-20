6578e3b293bfd273d28817941fe7fa48bd525c6d
ericamayo <161027018+ericamayo@users.noreply.github.com>
2024-05-20 22:34:26 +1000

    Basic Stacked Bar chart + updated csvs
    Bar chart working with drop down selection of countries, showing tobacco and vaping data where available
    Modified csv to include vaping and tobacco in one and updated mortalityCancer in prep for Sankey

--- scripts/stackedBar.js
+++ scripts/stackedBar.js
@@ -1,68 +1,129 @@
 //Width and height
-var w = 500;
+var w = 800;
 var h = 300;
 
-//dummy data
-var dataset = [
-    { apples: 5, oranges: 10, grapes: 22 },
-    { apples: 4, oranges: 12, grapes: 28 },
-    { apples: 2, oranges: 19, grapes: 32 },
-    { apples: 7, oranges: 23, grapes: 35 },
-    { apples: 23, oranges: 17, grapes: 43 }
-];
+//Mike Bostock Margin Convention - https://observablehq.com/@d3/margin-convention
+margin = ({top: 20, right: 60, bottom: 30, left: 60}) //can be further implemented for responsive
+    
 
-//Set up stack 
-var stack = d3.stack()
-    .keys(["apples", "oranges", "grapes"])
-    .order(d3.stackOrderDescending);  // <-- Flipped stacking order
+d3.csv("../data/VapingTobacco.csv").then((data) => {
 
-//Data, stacked
-var series = stack(dataset);
+    var countries = [...new Set(data.map(function(d) { return d.Country; }))];
+    countries.sort();
 
-//Set up scales
-var xScale = d3.scaleBand()
-    .domain(d3.range(dataset.length))
-    .range([0, w])
-    .paddingInner(0.05);
+    // Create dropdown list --- https://stackoverflow.com/questions/42209058/making-a-drop-down-menu-from-csv-data-in-js
+    var dropdown = d3.select(".dropdown-country")
+    .append("select") //html select element
+    .attr("id", "countrySelect")
+    .selectAll("option") // add to countries list
+    .data(countries) // bind to option
+    .enter()
+    .append("option") //display options
+    .text(function(d) { return d; }); //display names of options
 
-var yScale = d3.scaleLinear()
-    .domain([0,
-        d3.max(dataset, function (d) {
-            return d.apples + d.oranges + d.grapes;
+    // Event listener of dropdown list
+    d3.select("#countrySelect").on("change", function() {
+    var selectedCountry = d3.select(this).property("value");
+
+        // Filter rows and select columns
+        var filteredData = data.filter(function(d) {
+            return d.Sex == "Total" && d.Country == selectedCountry; //filter based on selected country (Sex can be further implemented for further interactivity)
+        }).map(function(d) {
+            return {
+                Year: d.Year,
+                ObservedPersons: +d["Observed Persons"],
+                VapingObservedPersons: +d["Vaping Observed Persons"]
+            };
+        });
+
+        // Sort filteredData by Year
+        filteredData.sort(function(a, b) {
+            return d3.ascending(a.Year, b.Year);
+        
+        });
+
+    //Set up stack 
+    var stack = d3.stack()
+        .keys(["ObservedPersons", "VapingObservedPersons"]);
+
+    //Data, stacked
+    var series = stack(filteredData);
+
+    //Set up scales
+    var xScale = d3.scaleBand()
+        .domain(filteredData.map(function(d) { return d.Year; }))
+        .range([margin.left, w - margin.right]) //range for visualisation
+        .padding(0.1); // Add padding between bars
+
+    var yScale = d3.scaleLinear()
+        .domain([0,
+            d3.max(series, function (d) {
+                return d3.max(d, function(d) { return d[1]; }); 
+            })
+        ])
+        .range([h - margin.bottom, margin.top]); //range for visualisation of screen based on SVG canvas
+
+    //Easy colors accessible via a 10-step ordinal scale
+    var colors = d3.scaleOrdinal(d3.schemeCategory10);
+
+    //Remove previous SVG element
+    d3.select(".vis3 svg").remove();
+
+
+    //Create SVG element
+    var svg = d3.select(".vis3")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+    // Add a group for each row of data
+    var groups = svg.selectAll("g")
+        .data(series)
+        .enter()
+        .append("g")
+        .style("fill", function (d, i) {
+            return colors(i);
+        });
+
+    // Add a rect for each data value
+    var rects = groups.selectAll("rect")
+        .data(function (d) { return d; })
+        .enter()
+        .append("rect")
+        .attr("x", function (d, i) {
+            return xScale(d.data.Year);
         })
-    ])
-    .range([h, 0]);  // <-- Flipped vertical scale
+        .attr("y", function (d) {
+            return yScale(d[1]);  // <-- Changed y value
+        })
+        .attr("height", function (d) {
+            return yScale(d[0]) - yScale(d[1]);  // <-- Changed height value
+        })
+        .attr("width", xScale.bandwidth());
 
-//Easy colors accessible via a 10-step ordinal scale
-var colors = d3.scaleOrdinal(d3.schemeCategory10);
 
-//Create SVG element
-var svg = d3.select(".vis3")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+    //https://ghenshaw-work.medium.com/customizing-axes-in-d3-js-99d58863738b
+    var xAxis = d3.axisBottom()
+    // .select(".domain").remove()
+    .scale(xScale)
+    .tickSize(0);
 
-// Add a group for each row of data
-var groups = svg.selectAll("g")
-    .data(series)
-    .enter()
-    .append("g")
-    .style("fill", function (d, i) {
-        return colors(i);
+    var yAxis = d3.axisLeft()
+        .scale(yScale)
+        .ticks(6)
+        // .tickSize(0)
+        .tickFormat(d3.format("~s")); //https://github.com/d3/d3/blob/45df8c66dfe43ad0824701f749a9bf4e3562df85/docs/d3-format.md?plain=1
+
+    svg.append("g") //group x axis elements
+        .attr("class", "axis") // assign class (name group)
+        .attr("transform", "translate(0," + (h - margin.bottom) + ")") //move x axis to bottom of SVG
+        .call(xAxis)
+        .select(".domain").remove();
+
+    svg.append("g") //group y axis elements
+        .attr("class", "axis") // assign class (name group)
+        .attr("transform", "translate(" + (margin.left) + ",0)") //move y axis to left of SVG
+        .call(yAxis)
+        .select(".domain").remove();
     });
-
-// Add a rect for each data value
-var rects = groups.selectAll("rect")
-    .data(function (d) { return d; })
-    .enter()
-    .append("rect")
-    .attr("x", function (d, i) {
-        return xScale(i);
-    })
-    .attr("y", function (d) {
-        return yScale(d[1]);  // <-- Changed y value
-    })
-    .attr("height", function (d) {
-        return yScale(d[0]) - yScale(d[1]);  // <-- Changed height value
-    })
-    .attr("width", xScale.bandwidth());
+});
